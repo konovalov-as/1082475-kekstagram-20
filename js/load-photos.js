52fd203bb2967670954d61d77ef60e3602fd74d8
Konovalov Alexander <konovalov_a.s@mail.ru>
2020-07-22 16:59:39 +0500

    Добавляет фильтрацию фотографий

--- js/load-photos.js
+++ js/load-photos.js
@@ -33,6 +33,7 @@
     photoContainer.addEventListener('click', function (evt) {
       window.view.onPhotoContainerClick(evt, photos);
     });
+    filter(photos);
   };
 
   // display an error message
@@ -44,5 +45,61 @@
   };
 
   window.backend.load(onLoad, onError);
+
+  var filterContainer = document.querySelector('.img-filters');
+  var filterForm = filterContainer.querySelector('.img-filters__form');
+
+  var filter = function (photos) {
+    filterContainer.classList.remove('img-filters--inactive');
+
+    var onFilterFormClick = function (evt) {
+      if (!evt.target.matches('.img-filters__button')) {
+        return;
+      }
+
+      var getRandomNumber = function (min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min; // Максимум и минимум включаются
+      };
+
+      var filterPhotos = [];
+      if (evt.target.matches('#filter-default')) {
+        filterPhotos = photos.slice();
+      }
+
+      if (evt.target.matches('#filter-random')) {
+        while (filterPhotos.length < 10) {
+          var randomNumber = getRandomNumber(0, photos.length - 1);
+          if (!filterPhotos.includes(photos[randomNumber])) {
+            filterPhotos.push(photos[randomNumber]);
+          }
+        }
+      }
+
+      if (evt.target.matches('#filter-discussed')) {
+        var photosCopy = photos.slice();
+        photosCopy.sort(function (first, second) {
+          if (first.comments.length < second.comments.length) {
+            return 1;
+          }
+          if (first.comments.length > second.comments.length) {
+            return -1;
+          }
+          return 0;
+        });
+
+        filterPhotos = photosCopy;
+      }
+
+      var pictures = photoContainer.querySelectorAll('.picture');
+      pictures.forEach(function (picture) {
+        picture.remove();
+      });
+
+      renderPhotos(filterPhotos);
+    };
+    filterForm.addEventListener('click', onFilterFormClick);
+  };
 
 })();
